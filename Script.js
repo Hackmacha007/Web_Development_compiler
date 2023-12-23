c23959605a390afb81e7c216dad8ded945062164
Raghavendra Goud <61324448+Hackmacha007@users.noreply.github.com>
2023-12-23 18:25:06 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,19 @@
+function compileCode() {
+  const htmlCode = document.getElementById('html-editor').value;
+  const cssCode = document.getElementById('css-editor').value;
+  const jsCode = document.getElementById('js-editor').value;
+
+  const outputFrame = document.getElementById('output-frame');
+  const outputDocument = outputFrame.contentDocument || outputFrame.contentWindow.document;
+
+  outputDocument.open();
+  outputDocument.write(`
+      <html>
+      <head>
+          <style>${cssCode}</style>
+      </head>
+      <body>${htmlCode}<script>${jsCode}</script></body>
+      </html>
+  `);
+  outputDocument.close();
+}
