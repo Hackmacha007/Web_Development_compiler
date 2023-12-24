86aeaa2599c96f58b505752ffad52a77ed96a6e8
Raghavendra Goud <61324448+Hackmacha007@users.noreply.github.com>
2023-12-24 17:28:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
+// function compileCode() {
+//   const htmlCode = document.getElementById('html-editor').value;
+//   const cssCode = document.getElementById('css-editor').value;
+//   const jsCode = document.getElementById('js-editor').value;
+
+//   const outputFrame = document.getElementById('output-frame');
+//   const outputDocument = outputFrame.contentDocument || outputFrame.contentWindow.document;
+
+//   outputDocument.open();
+//   outputDocument.write(`
+//       <html>
+//       <head>
+//           <style>${cssCode}</style>
+//       </head>
+//       <body>${htmlCode}<script>${jsCode}</script></body>
+//       </html>
+//   `);
+//   outputDocument.close();
+// }
+
+
+// script.js
+
 function compileCode() {
-  const htmlCode = document.getElementById('html-editor').value;
-  const cssCode = document.getElementById('css-editor').value;
-  const jsCode = document.getElementById('js-editor').value;
+    // Get the values from the textareas
+    var htmlCode = document.getElementById('html-editor').value;
+    var cssCode = document.getElementById('css-editor').value;
+    var jsCode = document.getElementById('js-editor').value;
 
-  const outputFrame = document.getElementById('output-frame');
-  const outputDocument = outputFrame.contentDocument || outputFrame.contentWindow.document;
+    // Combine the code into a full HTML document
+    var fullCode = '<html><head><style>' + cssCode + '</style></head><body>' + htmlCode + '<script>' + jsCode + '</script></body></html>';
 
-  outputDocument.open();
-  outputDocument.write(`
-      <html>
-      <head>
-          <style>${cssCode}</style>
-      </head>
-      <body>${htmlCode}<script>${jsCode}</script></body>
-      </html>
-  `);
-  outputDocument.close();
+    // Open a new window and display the output in an alert
+    var newWindow = window.open();
+    newWindow.alert(fullCode);
 }
